c8d299c5774b6ab330e1feeedbae2c5fdc8c08eb
Lyall <me@lyall.lol>
2025-05-30 01:15:08 +0100

    mindex

--- create_fmim.js
+++ create_fmim.js
@@ -1,26 +1,25 @@
 const fs = require("fs");
-const cp = require("child_process");
 
 if (!fs.existsSync("./mindex")) fs.mkdirSync("./mindex");
 
 const wmaFile = fs.readFileSync("./test_audio.wma");
 
 const fmimFile = Buffer.alloc(3336 + wmaFile.byteLength);
+let fmimOffset = 0;
 
-let offset = 0;
-fmimFile.write("FMIM", offset); offset += 4; // Magic
-fmimFile.writeBigUInt64BE(0x0000000100010001n, offset); offset += 8; // Unknown
-offset += 1; // x360 music organizer does this but i swear its wrong???????
-fmimFile.write("This is the name of the track", offset, "utf-16le"); offset += 512; // Track
-fmimFile.write("This is the name of the album", offset, "utf-16le"); offset += 512; // Album
-fmimFile.write("This is the name of the artist", offset, "utf-16le"); offset += 512; // Artist
-fmimFile.write("This is the name of the artist", offset, "utf-16le"); offset += 512; // Artist (?)
-fmimFile.write("This is the name of the genre", offset, "utf-16le"); offset += 512; // Genre
-fmimFile.write("This is the name of the genre", offset, "utf-16le"); offset += 512; // Genre (?)
-fmimFile.writeUInt32LE(69000, offset); offset += 4; // Track length (ms)
-fmimFile.writeUInt32LE(69, offset); offset += 4; // Track number (starts from 1)
-offset += 243; // Unknown - meant to be 244
-wmaFile.copy(fmimFile, offset); offset += wmaFile.byteLength;
+fmimFile.write("FMIM", fmimOffset); fmimOffset += 4; // Magic
+fmimFile.writeBigUInt64BE(0x0000000100010001n, fmimOffset); fmimOffset += 8; // Unknown
+fmimOffset += 1; // x360 music organizer does this but i suspect its wrong???????
+fmimFile.write("Test", fmimOffset, "utf-16le"); fmimOffset += 512; // Track
+// fmimFile.write("Album", fmimOffset, "utf-16le"); fmimOffset += 512; // Album
+fmimFile.write("Artist", fmimOffset, "utf-16le"); fmimOffset += 512; // Artist
+// fmimFile.write("Artist", fmimOffset, "utf-16le"); fmimOffset += 512; // Artist (?)
+// fmimFile.write("Genre", fmimOffset, "utf-16le"); fmimOffset += 512; // Genre
+// fmimFile.write("Genre", fmimOffset, "utf-16le"); fmimOffset += 512; // Genre (?)
+// fmimFile.writeUInt32LE(281000, fmimOffset); fmimOffset += 4; // Track length (ms)
+// fmimFile.writeUInt32LE(1, fmimOffset); fmimOffset += 4; // Track number (starts from 1)
+fmimOffset += 243; // Unknown - meant to be 244
+wmaFile.copy(fmimFile, fmimOffset); fmimOffset += wmaFile.byteLength;
 
 fs.mkdirSync("./mindex/media/0000", { recursive: true });
-fs.writeFileSync("./mindex/media/0000/0000", fmimFile);
+fs.writeFileSync("./mindex/media/0000/0008", fmimFile);
--- create_mindex.js
+++ create_mindex.js
@@ -1,1 +1,34 @@
-// this file isnt really documented anywhere online so im probably on my own for this one
+const fs = require("fs");
+
+const mindexTemplate = Buffer.from(require("./mindex.js"));
+
+if (!fs.existsSync("./mindex")) fs.mkdirSync("./mindex");
+
+const mindexFile = Buffer.alloc(6603);
+let mindexOffset = 0;
+
+mindexTemplate.copy(mindexFile, mindexOffset);
+
+const album = Buffer.alloc(82);
+album.write("Album", 0, "utf-16le");
+
+const title = Buffer.alloc(82);
+title.write("Test", 0, "utf-16le");
+
+const artist = Buffer.alloc(82);
+artist.write("Artist", 0, "utf-16le");
+
+const genre = Buffer.alloc(82);
+genre.write("Genre", 0, "utf-16le");
+
+album.copy(mindexFile, 4217);
+title.copy(mindexFile, 4817);
+artist.copy(mindexFile, 5417);
+genre.copy(mindexFile, 6017);
+// mindexFile.write("A album", 1079, "utf-16le");
+// mindexFile.write("Test", 4817, "utf-16le");
+// mindexFile.write("A artist", 5417, "utf-16le");
+// mindexFile.write("A genre", 6017, "utf-16le");
+mindexFile.writeUIntBE(281000 * 2, 4944, 3);
+
+fs.writeFileSync("./mindex/mindex.xmi", mindexFile);
