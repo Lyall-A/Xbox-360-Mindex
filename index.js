74012a506f614788df2a68810b09305eb9c96cb9
Lyall <me@lyall.lol>
2025-05-31 06:36:17 +0100

    gsmall changes

--- index.js
+++ index.js
@@ -5,38 +5,31 @@ const cp = require("child_process");
 const createMindexBuffer = require("./utils/createMindexBuffer");
 const createFmimBuffer = require("./utils/createFmimBuffer");
 
-const {
-    albumTitle,
-    artistTitle,
-    genreTitle,
-    titleFormat,
-    ffmpegPath,
-    tracksPath,
-    mindexOutput,
-    supportedFormats,
-    pipeOutput // pipe doesn't work btw
-} = require("./config.json");
+const config = require("./config.json");
 
-const tracks = fs.readdirSync(tracksPath).filter(file => supportedFormats.includes(path.extname(file).toLowerCase().substring(1))).map(file => ({ path: path.resolve(tracksPath, file) }));
+const tracks = fs.readdirSync(config.tracksPath).filter(file => !fs.statSync(path.join(config.tracksPath, file)).isDirectory()).map(file => ({ path: path.resolve(config.tracksPath, file) }));
+// const tracks = fs.readdirSync(tracksPath).filter(file => supportedFormats.includes(path.extname(file).toLowerCase().substring(1))).map(file => ({ path: path.resolve(config.tracksPath, file) }));
 
-const fmimPath = path.join(mindexOutput, "media/0000");
-if (fs.existsSync(mindexOutput)) fs.rmSync(mindexOutput, { recursive: true, force: true });
+const fmimPath = path.join(config.mindexOutput, "media/0000");
+if (fs.existsSync(config.mindexOutput)) fs.renameSync(config.mindexOutput, `${config.mindexOutput}_${Date.now()}`);
 if (!fs.existsSync(fmimPath)) fs.mkdirSync(fmimPath, { recursive: true });
 
 (function convertTrack(trackIndex) {
     const track = tracks[trackIndex];
-    const wmaPath = path.join(tracksPath, `${path.basename(track.path, path.extname(track.path))}.wma`);
+    const wmaPath = path.join(config.tracksPath, `temp/${path.basename(track.path, path.extname(track.path))}.wma`);
+    if (!fs.existsSync(path.dirname(wmaPath))) fs.mkdirSync(path.dirname(wmaPath), { recursive: true });
     
     console.log(`Converting track ${trackIndex + 1} of ${tracks.length} (${path.basename(track.path)})...`);
 
-    const ffmpegProcess = cp.spawn(ffmpegPath, [
+    const ffmpegProcess = cp.spawn(config.ffmpegPath, [
         "-i", track.path,
         "-c:a", "wmav2",
         "-b:a", "192k",
         "-ar", "44100",
         "-compression_level", "0",
         "-ac", "2",
-        ...(pipeOutput ? [
+        "-vn",
+        ...(config.pipeOutput ? [
             // Output to pipe (doesn't work)
             "-f", "asf",
             "-"
@@ -53,32 +46,40 @@ if (!fs.existsSync(fmimPath)) fs.mkdirSync(fmimPath, { recursive: true });
     ffmpegProcess.stderr.on("data", data => ffmpegOutputBufferArray.push(...data));
     ffmpegProcess.stdout.on("data", data => wmaBufferArray.push(...data));
 
-    ffmpegProcess.on("exit", () => {
+    ffmpegProcess.on("exit", exitCode => {
         const ffmpegOutput = Buffer.from(ffmpegOutputBufferArray).toString();
         const wmaBuffer = Buffer.from(wmaBufferArray);
 
-        // console.log(ffmpegOutput);
-
         const durationMatch = ffmpegOutput.match(/duration: ([\d.]+):([\d.]+):([\d.]+)/i);
-        if (!durationMatch) return console.log("Failed to get duration");
 
-        track.title = ffmpegOutput.match(/(?<=title           : ).*/i)?.[0];
-        track.artist = ffmpegOutput.match(/(?<=artist          : ).*/i)?.[0];
-        track.album = ffmpegOutput.match(/(?<=album           : ).*/i)?.[0];
-        track.genre = ffmpegOutput.match(/(?<=genre           : ).*/i)?.[0];
+        if (exitCode !== 0) {
+            if (!config.pipeOutput) fs.rmSync(wmaPath);
+            return console.log(`Failed to convert track! FFmpeg log:\n${ffmpegOutput}`);
+        }
+
+        if (!durationMatch) {
+            if (!config.pipeOutput) fs.rmSync(wmaPath);
+            return console.log("Failed to get track duration!");
+        }
+
+        track.title = ffmpegOutput.match(/(?<=title           : ).*/i)?.[0] || config.defaultTitle || path.basename(track.path, path.extname(track.path));
+        track.artist = ffmpegOutput.match(/(?<=artist          : ).*/i)?.[0] || config.defaultArtist;
+        track.album = ffmpegOutput.match(/(?<=album           : ).*/i)?.[0] || config.defaultAlbum;
+        track.genre = ffmpegOutput.match(/(?<=genre           : ).*/i)?.[0] || config.defaultGenre;
+        track.track = parseInt(ffmpegOutput.match(/(?<=track           : ).*/i)?.[0]) || 1;
         track.length = parseFloat(durationMatch[1]) * 3600000 + parseFloat(durationMatch[2]) * 60000 + parseFloat(durationMatch[3]) * 1000;
-        track.formattedTitle = titleFormat.replace(/{title}/g, track.title).replace(/{artist}/g, track.artist).replace(/{album}/g, track.album).replace(/{genre}/g, track.genre);
+        track.formattedTitle = config.titleFormat.replace(/{title}/g, track.title || "").replace(/{artist}/g, track.artist || "").replace(/{album}/g, track.album || "").replace(/{genre}/g, track.genre || "").replace(/{track}/g, track.track || "");
 
-        const fmimBuffer = createFmimBuffer(pipeOutput ? wmaBuffer : fs.readFileSync(wmaPath), track, trackIndex + 1);
+        const fmimBuffer = createFmimBuffer(config.pipeOutput ? wmaBuffer : fs.readFileSync(wmaPath), track);
         fs.writeFileSync(path.join(fmimPath, (!trackIndex ? 8 : 10 + trackIndex).toString(16).padStart(4, "0").toUpperCase()), fmimBuffer);
-        if (!pipeOutput) fs.rmSync(wmaPath);
+        if (!config.pipeOutput) fs.rmSync(wmaPath);
 
         if (tracks[++trackIndex]) {
             return convertTrack(trackIndex);
         } else {
-            console.log(`Creating mindex.xmi file...`);
-            const mindexBuffer = createMindexBuffer(albumTitle, artistTitle, genreTitle, tracks);
-            fs.writeFileSync(path.join(mindexOutput, "mindex.xmi"), mindexBuffer);
+            console.log(`Generating mindex.xmi file...`);
+            const mindexBuffer = createMindexBuffer(config.albumTitle, config.artistTitle, config.genreTitle, tracks);
+            fs.writeFileSync(path.join(config.mindexOutput, "mindex.xmi"), mindexBuffer);
         }
     });
 })(0);
--- utils/createFmimBuffer.js
+++ utils/createFmimBuffer.js
@@ -1,4 +1,4 @@
-function createFmimBuffer(wmaBuffer, track, trackNum) {
+function createFmimBuffer(wmaBuffer, track) {
     const fmimBuffer = Buffer.alloc(3336 + wmaBuffer.byteLength);
     
     fmimBuffer.write("FMIM", 0); // Magic
@@ -6,13 +6,13 @@ function createFmimBuffer(wmaBuffer, track, trackNum) {
     // x360 Music Organizer offsets by an extra 1 byte, I suspect that is incorrect
     // These aren't actually required
     fmimBuffer.write(track?.title || "", 12, "utf-16le"); // Track
-    // fmimBuffer.write(track?.album || "", 524, "utf-16le"); // Album
+    fmimBuffer.write(track?.album || "", 524, "utf-16le"); // Album
     fmimBuffer.write(track?.artist || "", 1036, "utf-16le"); // Artist
-    // fmimBuffer.write(track?.artist || "", 1548, "utf-16le"); // Artist (?)
-    // fmimBuffer.write(track?.genre || "", 2060, "utf-16le"); // Genre
-    // fmimBuffer.write(track?.genre || "", 2572, "utf-16le"); // Genre (?)
-    // fmimBuffer.writeUInt32LE(track?.length, 3084); // Track length (ms)
-    // fmimBuffer.writeUInt32LE(trackNum, 3088); // Track number (starts from 1)
+    fmimBuffer.write(track?.artist || "", 1548, "utf-16le"); // Artist (?)
+    fmimBuffer.write(track?.genre || "", 2060, "utf-16le"); // Genre
+    fmimBuffer.write(track?.genre || "", 2572, "utf-16le"); // Genre (?)
+    fmimBuffer.writeUInt32LE(track?.length, 3084); // Track length (ms)
+    fmimBuffer.writeUInt32LE(track?.track, 3088); // Track number (starts from 1)
     // 244 bytes of unknown data here
     wmaBuffer.copy(fmimBuffer, 3336);
 
